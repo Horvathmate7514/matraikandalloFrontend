5e9242fccd77a81f87f194e361ee70a60a174a77
Horvathmate7514 <hmate683@gmail.com>
2024-06-27 18:59:54 +0200

    matraikandallofrontend

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterView } from "vue-router";
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-bgcolor">
+    <div class="container-fluid mx-5">
+      <img src="/src/assets/img/logogold.png" alt="" style="width: 150px; height: 120px">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto ">
+          <li class="nav-item">
+             <router-link class="nav-link active" to="/">Főoldal</router-link>
+
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="#">Szolgáltatásaink</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="#">Árak</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              data-bs-toggle="dropdown" aria-expanded="false">
+              Company
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Blog</a></li>
+              <li><a class="dropdown-item" href="#">About Us</a></li>
+              <li><a class="dropdown-item" href="#">Contact us</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <router-view />
+</template>
+
+<style scoped>
+.navbar-bgcolor {
+  background-color: rgba(52, 50, 48, 255);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,86 @@
+<script setup>
+import AppFooter from '../components/AppFooter.vue';
+</script>
+
+<template>
+  <body>
+    <div class="row mb-4 mb-3">
+      <div class="col-md-8">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="/src/assets/img/kandallo.jpg" class="d-block w-50 mx-auto" alt="Slide 1">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="/src/assets/img/kemence.jpg" class="d-block w-50 mx-auto" alt="Slide 2">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Az ön igényeire szabva</h5>
+                <p>Kemence, grillező, bográcsozó és vaslapos platni.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="teglakalyha">
+                <img src="/src/assets/img/teglakalyha_2.jpeg" class="d-block w-50 mx-auto" alt="Slide 3">
+              </div>
+              <div class="carousel-caption d-none d-md-block teglakalyhacaption">
+                <h5>Az ön igényeire szabva</h5>
+                <p>Vakolt-és téglakályhák.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <p style="color: white; text-align:justify; font-size: 20px; margin-top: 250px; margin-right: 100px;">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quam quas nam. Eveniet provident molestias
+          exercitationem? Fuga ullam a quam alias sit quod cum iusto excepturi in voluptas! Fugiat, cumque.
+        </p>
+      </div>
+    </div>
+    <AppFooter />
+  </body>
+</template>
+
+<style scoped>
+body {
+  min-height: 100vh;
+  background: #343331
+}
+
+.carousel-item img {
+  object-fit: contain;
+  max-height: 100%;
+  min-height: 90vh;
+}
+
+.carousel-control-prev {
+  margin-left: 250px;
+}
+
+.carousel-control-next {
+  margin-right: 250px;
+}
+
+.teglakalyha {
+  height: 100%;
+  align-items: center;
+
+}
+.teglakalyhacaption{
+  bottom: 10rem!important;
+}
+</style>
