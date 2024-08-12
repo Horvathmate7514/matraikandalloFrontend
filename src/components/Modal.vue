e7b03070629cf2118a50880037461993028e1054
sunrab1822 <wbarnus99@gmail.com>
2024-08-12 18:59:58 +0200

    changes on modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="modal fade "  tabindex="-1" id="photoGaleryModal" data-bs-theme="dark">
+    <div class="modal fade " tabindex="-1" id="photoGaleryModal" data-bs-theme="dark">
         <div class="modal-dialog modal-lg">
             <div class="modal-content darker-bg">
-               
-                
+
+
                 <div class="modal-body">
-                    <button type="button" class="btn-close position-absolute top-0 end-0 m-2 border bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                    <slot/>
+                    <button type="button" class="btn-close position-absolute top-0 end-0 m-2 border bg-white"
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button @click="pageup(id)" type="button"
+                        class="bg-blur fa fa-chevron-right position-absolute top-50 end-0 m-2 p-3"></button>
+                    <button type="button"
+                        class="bg-blur fa fa-chevron-left position-absolute top-50 start-0 m-2 p-3"></button>
+
+                        <img :src="gallery[imageId]" class="w-100">
                 </div>
             </div>
         </div>
@@ -14,5 +20,30 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const props = defineProps(["imageId", "gallery"]);
+let page = 0
+let id = props.imageId + page;
+
+const pageup = (thisId) =>{
+    console.log(id)
+    
+    
+}
 
 </script>
+
+<style scoped>
+.bg-blur {
+    border: none;
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 30%;
+    backdrop-filter: blur(2px);
+}
+.bg-blur:hover {
+    background-color: rgba(255, 255, 255, 0.4);
+}
+
+
+</style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,38 +1,37 @@
 <template>
 
    <div class="">
-         <div class="d-flex justify-content-center flex-wrap  gap-3">
-            <div v-for="item in 15" class="p-3 col-3 border border-dark rounded-3 darker-bg">
-               <img src="../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg" alt=""
-                  class="w-100">
-            </div>
-            <div  class="p-3 col-3 d-flex align-items-center border border-dark rounded-3 darker-bg">
-               <img src="../assets/img/T.K1.jpg" alt=""
-                  class="w-100">
-            </div>
-            <div v-for="item in 15" class="p-3 col-3 border border-dark rounded-3 darker-bg" 
-            @click="modalImage()" data-bs-toggle="modal" data-bs-target="#photoGaleryModal">
-               <img src="../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg" alt=""
-                  class="w-100" >
-            </div>
-            
+      <div class="d-flex justify-content-center flex-wrap  gap-3">
+         <!-- <div class="p-3 col-3 d-flex align-items-center border border-dark rounded-3 darker-bg">
+            <img src="../assets/img/T.K1.jpg" alt="" class="w-100">
+         </div> -->
+         <div v-for="(item, key) in gallery" class="p-3 col-3 border border-dark rounded-3 darker-bg" @click="modalImage(key)"
+            data-bs-toggle="modal" data-bs-target="#photoGaleryModal">
+            <img :src="item" alt="" class="w-100">
+         </div>
       </div>
    </div>
-   <Modal>
-      <img src="../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg" class="w-100">
-   </Modal>
+   <Modal :imageId="selectedImageIndex" :gallery="gallery" />
+   <!-- <img src="../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg" class="w-100"> -->
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import Modal from '../components/Modal.vue';
-   const selectedImage = ref('');
+const selectedImageIndex = ref('');
+const modules = import.meta.glob('@/assets/img/**/*')
+const gallery = []
+for (const path in modules) {
+   const p = new URL(path, import.meta.url).href;
+   gallery.push(p);
+}
 
-const modalImage =() =>{
-   selectedImage.value = '../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg';
-   console.log(selectedImage.value);
+const modalImage = (id) => {
+   selectedImageIndex.value = id;
+   console.log(selectedImageIndex.value);
    
 }
+
 </script>
 
 <style scoped></style>
