089e8b704a65fa01f5bc8061d2b6f9932a397741
sunrab1822 <wbarnus99@gmail.com>
2024-08-12 16:08:14 +0200

    now modal looks great

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="modal fade"  tabindex="-1" id="photoGaleryModal">
+    <div class="modal fade "  tabindex="-1" id="photoGaleryModal" data-bs-theme="dark">
         <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
+            <div class="modal-content darker-bg">
+               
+                
                 <div class="modal-body">
+                    <button type="button" class="btn-close position-absolute top-0 end-0 m-2 border bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
                     <slot/>
                 </div>
             </div>
