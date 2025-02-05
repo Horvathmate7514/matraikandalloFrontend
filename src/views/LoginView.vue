002175103d8328bed7c3efa5d5066c8ceb8d718d
Horvathmate7514 <hmate683@gmail.com>
2025-02-05 19:56:41 +0100

    loginview

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="wrapper">
-        <h2>Bejelentkezés</h2>
-
-        <div class="input-box">
-            <input type="email" class="form-control" id="email" placeholder="E-mail" />
+  <div class="d-flex justify-content-center align-items-center">
+    <div class=" p-4 shadow-lg">
+      <h2 class="text-center">Bejelentkezés</h2>
+      <form >
+        <div class="input-box mb-3">
+          <input v-model="email" type="email" class="form-control" id="email" placeholder="E-mail" required />
         </div>
-        <div class="input-box">
-            <input type="password" class="form-control" id="password" placeholder="Jelszó" />
+        <div class="input-box mb-3">
+          <input v-model="password" type="password" class="form-control" id="password" placeholder="Jelszó" required />
         </div>
-        <div class="input-box button">
-            <input type="Submit" value="Bejelentkezés" />
+        <div class="input-box button mb-3">
+          <input type="submit" class="logingomb btn w-100" value="Bejelentkezés" />
         </div>
+      </form>
     </div>
+  </div>
 
 </template>
 
-<script setup></script>
+<script setup>
+</script>
+
+<style scoped>
+.logingomb{
+    background-color: #ecd089;
+
+}
+</style>
