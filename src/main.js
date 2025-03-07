4224411168d80c46dd707f05761a533175616c73
sunrab1822 <wbarnus99@gmail.com>
2025-03-07 20:07:44 +0100

    login stuff

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
@@ -10,6 +12,7 @@ import './assets/css/style.css';
 const app = createApp(App);
 const pinia = createPinia()
 
+pinia.use(piniaPluginPersistedState)
 
 app.use(pinia)
 app.use(router);
