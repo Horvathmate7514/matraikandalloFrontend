ef8d1b45fe9e6ecd3f29203a0caa3e8e65e2fb24
sunrab1822 <wbarnus99@gmail.com>
2025-03-07 19:58:52 +0100

    del log line

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -2,18 +2,13 @@
 
    <div class="">
       <div class="d-flex justify-content-center flex-wrap  gap-3">
-         <!-- <div class="p-3 col-3 d-flex align-items-center border border-dark rounded-3 darker-bg">
-            <img src="../assets/img/T.K1.jpg" alt="" class="w-100">
-         </div> -->
          <div v-if="gallery != ''" v-for="(item, key) in gallery"
             class="p-3 col-sm-3 col-md-2 border border-dark rounded-3 darker-bg d-flex align-items-center " @click="modalImage(key)"> 
-            <!-- data-bs-toggle="modal" data-bs-target="#photoGaleryModal" -->
             <img :src="item" draggable="false" class="imgs selectDisable">
          </div>
       </div>
    </div>
-   <!-- <Modal :imageId="gallery[selectedImageIndex]" :gallery="gallery"/> -->
-   <!-- <img src="../assets/img/kandallo/79844871_2455984214662996_6265950529986232320_n.jpg" class="w-100"> -->
+
 </template>
 
 <script setup>
@@ -25,10 +20,7 @@ const gallery = ref()
 
 
 dataservice.getAllImages().then(data => {
-
    gallery.value = data.data
-   console.log(gallery.value);
-
 })
 
 
